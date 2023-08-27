e69d9aff4855d4b4faac45afa8e4a112ee7fe04a
Pavel Torbeev <pavel.trbv@gmail.com>
2023-08-27 07:22:06 +0300

    Обновил адрес бэкенда

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // export const BACKEND_URL = 'http://192.168.83.181:8000';
 // export const BACKEND_URL = 'http://192.168.22.4:8000';
 // export const BACKEND_URL = 'https://ed68-77-234-219-9.ngrok-free.app';
-export const BACKEND_URL = 'http://192.168.83.181';
+export const BACKEND_URL = 'https://9839-176-59-1-128.ngrok-free.app';
 
 export const API_URL = BACKEND_URL + '/api';
